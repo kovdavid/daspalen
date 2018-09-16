a5816b1817f4d9adfcad7d4e5345e1255cad3585
Dávid Kovács <kovdavid@gmail.com>
2018-09-16 07:37:32 +0200

    progress

--- app/src/main/java/com/github/davsx/llearn/activities/ManageCards/ManageCardsActivity.java
+++ app/src/main/java/com/github/davsx/llearn/activities/ManageCards/ManageCardsActivity.java
@@ -5,10 +5,17 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.github.davsx.llearn.LLearnApplication;
@@ -17,7 +24,7 @@ import com.github.davsx.llearn.service.ManageCards.ManageCardsService;
 
 import javax.inject.Inject;
 
-public class ManageCardsActivity extends Activity {
+public class ManageCardsActivity extends AppCompatActivity {
 
     private static final String TAG = "ManageCardsActivity";
 
@@ -35,18 +42,47 @@ public class ManageCardsActivity extends Activity {
 
         ((LLearnApplication) getApplication()).getApplicationComponent().inject(this);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         adapter = new ManageCardsAdapter(this, manageCardsService);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
 
-        FloatingActionButton createCardButton = findViewById(R.id.create_card_button);
-        createCardButton.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.manage_cards_menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onClick(View view) {
-                showCreateCardDialog();
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                Toast.makeText(ManageCardsActivity.this, "onQueryTextChange "+newText, Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new_word:
+                showCreateCardDialog();
+            default:
+                break;
+        }
+
+        return true;
     }
 
     private void showCreateCardDialog() {
